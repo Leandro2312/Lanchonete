f1bc4447dd4693a44b513eaa758a5e3372ce44bf
Leandro2312 <94083259+Leandro2312@users.noreply.github.com>
2022-04-28 22:02:59 -0300

    Desenvolvimento cadastro produto e cadastro pedido no sql

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+
+// as Palavras começadas com $ são variáveis, aqui valem as mesmas regras para nome de variável (sem caractere especial, sem espaço, nomes siguinificativos, diferença de maiuscula e minuscula)
+
+
+$nome = "Leandro ";
+echo $nome;
+echo "<br>";
+echo "Seja Bem-Vindo {$nome} ao nosso servidor";
+
+phpinfo();
+
+?>
---
+++ tela-produtos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> Cadastro de Usuário </title>
+</head>
+<body>
+<form method="post" action="cadastro-produtos.php">
+    Produto:
+    <input name="Produto" id="produto">
+    <br>
+    Preço:    
+    <input name="preco" id="preco">
+    <br>
+    Ingredientes:
+    <textarea name="ingredientes" id="ingredientes"></textarea>
+    <br>
+    <button type ="submit">Salvar</button>
+</form>
+</body>
+</html>
