d225b7168043341c99f0ce22f450b0a41291415c
Leandro2312 <94083259+Leandro2312@users.noreply.github.com>
2022-04-28 22:09:04 -0300

    atualização no cadastro-produtos.php

--- cadastro-produtos.php
+++ cadastro-produtos.php
@@ -1,5 +1,17 @@
 <?php
 
-&conexao = mysqli_connect("localhost", "USUARIO", "SENHA", "BASE")
+&conexao = mysqli_connect("localhost", "root", "", "lanchonete")
 
-  
+$produto = $_POST ['produto'];
+$preco = $_POST ['preco'];
+$ingredientes = $_POST ['ingredientes'];
+echo "{$produto} - {$preco} - {$ingredientes}";
+
+
+
+
+
+
+
+  mysqli_close($conexao);
+  ?>
